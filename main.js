87ebd53c318a7a3451465da9d36d8ec0be27566a
CodeWithHarry <48705673+CodeWithHarry@users.noreply.github.com>
2023-08-22 16:35:57 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("Main.js working")
+
+const populate = async (value, currency) => {
+    let myStr = ""
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_7UStkUqQNBmahSoy8K635tE3Sjr5fK1UVPmVloZ2&base_currency=" + currency
+    let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display = "block"
+
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+                    </tr> 
+                `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
+
+}
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='currency']").value
+    populate(value, currency)
+})
+
